1c18ba1a0c44c228809bd9e50db03d50e1dff4e5
shur-complement <139090555+shur-complement@users.noreply.github.com>
2023-08-10 20:46:52 -0400

    minimalist bitset

---
+++ succinct/bitset.py
@@ -0,0 +1,40 @@
+from math import ceil
+
+BITS = 32
+
+class BitSet:
+
+    def __init__(self, nbits):
+        self.data = [0] * int(ceil(nbits / 32))
+
+    def __len__(self):
+        return len(self.data) * BITS
+
+    def set(self, i):
+        mask = 1 << (i % BITS)
+        idx = i // BITS
+        self.data[idx] |= mask
+
+    def get(self, i):
+        mask = 1 << (i % BITS)
+        idx = i // BITS
+        return self.data[idx] & mask > 0
+
+    def clear(self, i):
+        mask = 1 << (i % BITS)
+        idx = i // BITS
+        self.data[idx] &= ~mask
+
+if __name__ == "__main__":
+    bs = BitSet(32)
+    bs.set(0)
+    bs.set(1)
+    print(bs.get(0))
+    print(bs.get(1))
+    print(bs.get(2))
+    print(bs.get(31))
+    print(len(bs))
+    bs.clear(0)
+    bs.clear(1)
+    print(bs.get(0))
+    print(bs.get(1))
