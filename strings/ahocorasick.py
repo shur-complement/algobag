33c87eac4724ca4b76f239b719993651c9ca0098
shur-complement <139090555+shur-complement@users.noreply.github.com>
2023-08-15 23:52:44 -0400

    shhh....

--- strings/ahocorasick.py
+++ strings/ahocorasick.py
@@ -13,7 +13,7 @@ FAIL = -1
 EMPTY = 0
 class AhoCorasick:
     def __init__(self, words):
-        NUM_STATES = sum(len(word) for word in words)
+        NUM_STATES = 1 + sum(len(word) for word in words)
         self.go = [[FAIL] * ALPHABET_SIZE for _ in range(NUM_STATES)]
         self.fail = [FAIL] * NUM_STATES
         self.out = [EMPTY] * NUM_STATES
